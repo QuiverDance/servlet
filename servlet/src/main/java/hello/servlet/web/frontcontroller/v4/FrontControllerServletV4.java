a115515683867225941c394d93c4144d5773c5cb
여름 <pjw7200@ajou.ac.kr>
2023-09-02 16:10:13 +0900

    REFACTOR : revise version in url

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -21,9 +21,9 @@ public class FrontControllerServletV4 extends HttpServlet {
     private Map<String, ControllerV4> controllerMap = new HashMap<>();
 
     public FrontControllerServletV4() {
-        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV4());
-        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV4());
-        controllerMap.put("/front-controller/v2/members", new MemberListControllerV4());
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
     }
 
     @Override
